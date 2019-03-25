55e335b7fbdb4c920daedf5edf3b11f4f1aff485
AnkitRamtriya <ankitramtriya774@gmail.com>
2019-03-25 18:10:20 +0530

    cutom-ebook pojo created

--- custom-E-book/src/main/java/com/example/demo/ebook/model/customEBook/CustomEBook.java
+++ custom-E-book/src/main/java/com/example/demo/ebook/model/customEBook/CustomEBook.java
@@ -2,11 +2,42 @@ package com.example.demo.ebook.model.customEBook;
 
 import javax.persistence.*;
 
+import com.example.demo.ebook.model.buyer.Buyer;
+import com.example.demo.ebook.model.chapter.Chapter;
+
 @Entity
 public class CustomEBook {
 	@Id
 	@GeneratedValue
 	private int id;
 	private double price;
+	@ManyToOne
+	private Chapter chapter;
+	@ManyToOne
+	private Buyer buyer;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public Chapter getChapter() {
+		return chapter;
+	}
+	public void setChapter(Chapter chapter) {
+		this.chapter = chapter;
+	}
+	public Buyer getBuyer() {
+		return buyer;
+	}
+	public void setBuyer(Buyer buyer) {
+		this.buyer = buyer;
+	}
 
 }
